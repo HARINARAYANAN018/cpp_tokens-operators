559f37574ca183ab6437b9cce79ff7401c3aa164
HARINARAYANAN018 <79526544+HARINARAYANAN018@users.noreply.github.com>
2022-04-16 20:41:52 +0530

    sizeof_func
    To find the size of the datatype

---
+++ sizeof_func.cpp
@@ -0,0 +1,11 @@
+/* 1 program
+to find the size of the datatype in system*/
+#include<iostream>
+using namespace std;
+main()
+{
+	cout<<"The size of char: "<<sizeof(char)<<" byte"<<endl;
+	cout<<"The size of int: "<<sizeof(int)<<" byte"<<endl;
+	cout<<"The size of float: "<<sizeof(float)<<" byte"<<endl;
+	cout<<"The size of double: "<<sizeof(double)<<" byte"<<endl;
+}
